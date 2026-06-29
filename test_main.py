c70eb6442cc42da90e8b8166d0012e2c46eb3eb9
Mario Christian <mariochristiancode@gmail.com>
2026-06-29 16:35:24 +0700

    fix booking test to authenticate as admin_dj

--- test_main.py
+++ test_main.py
@@ -1,6 +1,10 @@
 from fastapi.testclient import TestClient
 from main import app
 from database import get_connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 client = TestClient(app)
 
@@ -10,12 +14,11 @@ def test_booking():
     cursor.execute("DELETE FROM BOOKING WHERE date = '2024-05-01'")
     conn.commit()
     conn.close()
-    response = client.post("/bookings", json={"dj_id": 1, "outlet_id": 1,"date": "2024-05-01"})
+    response = client.post("/login", json={"email": "hwadmin.dj@gmail.com", "password": os.getenv("ADMIN_DJ_PASSWORD")})
+    token = response.json()["token"]
+    response = client.post("/bookings", json={"dj_id": 1, "outlet_id": 1,"date": "2024-05-01"}, headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
     assert response.json() == {"message": "DJ booked successfully"}
-    response_2 = client.post("/bookings", json={"dj_id": 1, "outlet_id": 1, "date": "2024-05-01"})
+    response_2 = client.post("/bookings", json={"dj_id": 1, "outlet_id": 1, "date": "2024-05-01"}, headers={"Authorization": f"Bearer {token}"})
     assert response_2.status_code == 200
     assert response_2.json() == {"message": "DJ is already booked on this date"}
-
-
-
